e237816fdc96782a3b7131c0864e10b6ff7b4d9a
Benjamin <benjaminrowe0@gmail.com>
2022-04-30 12:05:54 +1000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+    attr_reader :name, :roster
+   def initialize(name)
+    @name = name
+    @roster = Hash.new { |hash,key| hash[key] =[]}
+   end 
+
+   def add_student(student, grade)
+       @roster[grade] << student
+   end
+
+   def grade(grade)
+        if(@roster.key?(grade))
+            @roster[grade]
+        end 
+   end 
+
+   def sort
+        @roster.sort
+   end
+
+   
+end
+
+high_school = School.new("Charls Highest")
+high_school.add_student("Jeff Baird", 10)
+high_school.add_student("Max Flombaum", 10)
+high_school.add_student("Avi Flombaum", 9)
+high_school.add_student("Bob tim", 3)
+
+
+# puts high_school.roster
+puts high_school.grade(10)
